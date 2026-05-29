aae89703977ffa07c4566466018f133257ef7930
harinath0608 <harinath192006@gmail.com>
2026-05-29 07:24:07 +0530

    task1 completed

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 console.log("Hello World")
 
 console.log("Javascript")
+
+// let, const, var
+
+let fruit = "Apple"
+
+console.log(fruit)
+
+let marks = 100 
+
+marks = 80 
+
+console.log(marks)
+
+
+var score = 200 
+var score = 300 
+
+score = 500 
+
+console.log(score)
+
+
+const runs = true
+
+// runs = 100 
+
+console.log( typeof runs)
+
+
+console.log( typeof names)
+
+console.error("Error Found")
+
+console.warn("Warning Message")
