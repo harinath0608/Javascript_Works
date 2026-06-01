626543994edf385d4225404ac6ed89e37b296484
harinath0608 <harinath192006@gmail.com>
2026-06-01 07:17:26 +0530

    operator & conditions

---
+++ Operator&Conditions/app.js
@@ -0,0 +1,72 @@
+// let age = 18 ;
+
+// if (age > 18 ) {
+//     console.log("Eligible to vote")
+// }
+
+// else if (age = 18) {
+//     console.log("Initiated Vote")
+// }
+
+// else {
+//     console.log("Not Eligible")
+// }
+
+
+let a = 20 
+let b = 15 
+let c = a + b
+console.log(c)
+
+let mobilePrice = 15000
+let quantity = 3 
+
+let totalPrice = mobilePrice * quantity 
+
+console.log(totalPrice);
+
+
+//  == loose equal 
+
+
+console.log(10>5);
+console.log(10>=10);
+
+
+console.log(100 === '100');
+
+console.log('100' === '100');
+
+
+
+// let age = prompt("")
+
+let age = 18 
+
+console.log(age > 18 && age < 25)
+
+console.log(age > 18 || age == 25)
+
+
+// Day Finder 
+
+let day = 4
+
+switch(day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+        default:
+        console.log("Invalid day");   
+}
+
+
+
+
+
