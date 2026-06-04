aee6623eabf3316d235ea9a003b88e6035f12366
harinath0608 <harinath192006@gmail.com>
2026-06-04 07:26:41 +0530

    scopes & functions

---
+++ Functions&Scopes/app.js
@@ -0,0 +1,58 @@
+
+// function greet(name, age) {
+//     console.log("Good Morning " + name + "" + age);
+// }
+// greet("Govind", 27)
+
+
+
+// function sub(a, b) {
+//    return (a - b)
+// }
+
+// let subtraction = sub(10, 5)
+
+// console.log(subtraction);
+
+
+
+// function mul(a, b) {
+//    return (a * b)
+// }
+
+// let multiplication = mul(10, 5)
+
+// console.log(multiplication);
+
+
+// const greet = () => {
+// console.log("Good Afternoon");
+// }
+
+// greet()
+
+
+// const add = (a,b) => {  return a+b ;}
+// console.log(add(10,19))
+
+
+
+// let college = "ABC college"
+
+// function show() {
+//     console.log(college)
+// }
+
+// show()
+
+
+
+
+function greet() {
+let name = "Hari"
+console.log(name);
+}
+greet()
+
+
+console.log(name)
