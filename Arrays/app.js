6e6f1324d8e0ebd3a1a8f29140b79876aaa30ebe
harinath0608 <harinath192006@gmail.com>
2026-06-09 06:58:55 +0530

    arrays

---
+++ Arrays/app.js
@@ -0,0 +1,71 @@
+// let fruits = ["apple", 10, "banana", true , 5.5]
+
+// console.log(fruits);
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+
+// console.log(fruits.length);
+
+
+// fruits.push("Orange")
+
+// console.log(fruits);
+
+// fruits.pop()
+
+// console.log(fruits);
+
+// fruits.pop()
+
+// console.log(fruits);
+
+
+// fruits.unshift("Orange")
+
+// console.log(fruits);
+
+// fruits.shift()
+
+// console.log(fruits);
+
+
+
+// Remove Elements
+let fruits = ["apple","banana","orange","kiwi"]
+
+// fruits.splice(1,0)
+
+// console.log(fruits);
+
+
+
+// // Add Elements
+// fruits.splice(1,1,"kiwi")
+
+// console.log(fruits);
+
+
+// Replace Elements
+// fruits.splice(1,1,"grapes")
+
+// console.log(fruits);
+
+
+// Slice Method (n-1)
+
+let specialfruits = fruits.slice(1,3)
+
+console.log(specialfruits);
+
+
+
+
+
+
+
+
+
