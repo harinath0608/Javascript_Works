c98120f4e87f22663edcbcfaa67bf8242fdf3f86
harinath0608 <harinath192006@gmail.com>
2026-06-11 06:53:00 +0530

    Objects

---
+++ Objects/app.js
@@ -0,0 +1,110 @@
+// let fruits = {
+//     name : "apple",
+//     count: 10,
+//     city: "Delhi"
+// }
+
+
+// console.log(fruits["city"]);
+
+
+// let cricket = {
+//     name: "Sachin",
+//     score: 105,
+//     runs: 1400
+// }
+
+// cricket.name = "Dhoni"
+// cricket.runs = 1200
+// cricket.city = "Mumbai"
+
+// delete cricket.runs 
+
+// console.log(cricket);
+
+
+// let students = {
+//     name : "Priya",
+//     address : {
+//         city: "Hyderabad",
+//         state : "Telangana"
+//     }
+// }
+
+// students.address.marks = 80 
+
+// console.log(students.address.marks);
+
+
+let student  = {
+    name: "Dhanush",
+    age: 25
+    // greet : function(a,b) {
+        
+    //     return a+b
+        
+    // }
+}
+
+
+// console.log(student.greet(2,3));
+
+// let {name,age} = student
+
+// console.log(name);
+// console.log(age);
+
+
+
+// let updatedRecord = {
+//     ...student,
+//     city: "Hyderabad",
+//     score: 120
+// }
+
+// console.log(updatedRecord);
+
+
+// let numbers = [1,2,3,4,5]
+
+// let updatedNumbers = [...numbers , 6,7,8]
+
+// console.log(updatedNumbers);
+
+
+
+
+// let fruits = ["apple", "orange", "banana", "kiwi"]
+
+// let [first, ...remainingNumbers] = fruits
+
+// console.log(first);
+
+// console.log(remainingNumbers);
+
+
+let firstName = ["aarya", "Singh"]
+
+let lastName = ["reddy","avtar"]
+
+
+let updatedName = [...firstName, ...lastName, "Dhoni"]
+
+console.log(updatedName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
