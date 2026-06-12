214477ea4abac7cf0b6b82fe68e97beae3c523b7
harinath0608 <harinath192006@gmail.com>
2026-06-12 20:31:24 +0530

    stringmethods

---
+++ String_Methods_Regex/app.js
@@ -0,0 +1,47 @@
+// let course = ["javascript", 10, "CSS", "SASS"]
+
+// console.log(course.length);
+
+
+// let email = "student@gmail.com"
+
+// console.log(email.includes("@"));
+
+
+let Mobile_Number = "+91-9177059193"
+
+console.log(Mobile_Number.includes("91"));
+
+
+let skills = "html,css,javascript, react"
+
+console.log(skills.length);
+
+
+let unique = skills.split(",")
+
+console.log(unique);
+
+console.log(unique.length);
+
+
+let interactive_skill = "I Love Java"
+
+
+let unique_skill = interactive_skill.replace("Love", "Hate")
+
+console.log(unique_skill);
+
+
+let spaces = "HTML  "
+
+console.log(spaces.length);
+
+console.log(spaces.trim().length);
+
+
+
+
+
+
+
