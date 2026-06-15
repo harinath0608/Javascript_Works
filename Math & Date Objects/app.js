6e52a6de6133ffde2cfb595c2a2540c67c9ad644
harinath0608 <harinath192006@gmail.com>
2026-06-15 07:04:04 +0530

    date & math objects

---
+++ Math & Date Objects/app.js
@@ -0,0 +1,31 @@
+// console.log(Math.PI);
+
+
+// console.log(Math.random())
+
+
+// let dice = Math.floor(Math.random()*6 + 1 )
+
+// console.log(dice)
+
+// let dice =Math.floor(Math.random()*9+1)
+
+// console.log(dice);
+
+
+let day = new Date()
+
+console.log(day)
+
+console.log(day.getFullYear())
+console.log(day.getDate())
+console.log(day.getDay)
+console.log(day.getHours())
+console.log(day.getTime)
+console.log(day.getMilliseconds())
+console.log(day.getMonth())
+
+
+let dateFormat = new Date()
+
+console.log(dateFormat.getDate() + "/" + ( dateFormat.getMonth()+1 ) + "/" + dateFormat.getFullYear() )
