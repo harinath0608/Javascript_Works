9f24d8b868479f085e9689b01d199086ae6becbc
harinath0608 <harinath192006@gmail.com>
2026-06-16 06:50:37 +0530

    updated

--- DOM/app.js
+++ DOM/app.js
@@ -1,14 +1,24 @@
 // const Heading = document.getElementById("title")
 // const Heading = document.querySelector("#title")
 // const Heading = document.querySelector(".Title")
-const Heading = document.querySelectorAll("h1")
+// const Heading = document.querySelectorAll("h1")
 
-Heading[0].innerText = "Query Selector All"
-Heading[0].style.color = "red"
+// Heading[0].innerText = "Query Selector All"
+// Heading[0].style.color = "red"
 
-Heading[1].innerText = "DOM = Js"
-Heading[1].style.color = "blue"
+// Heading[1].innerText = "DOM = Js"
+// Heading[1].style.color = "blue"
 
-Heading.innerText = "Dynamic Content = DOM"
-Heading.style.color = "white"
-Heading.style.backgroundColor = "red"
+// Heading.innerText = "Dynamic Content = DOM"
+// Heading.style.color = "white"
+// Heading.style.backgroundColor = "red"
+
+
+const heading = document.getElementById("title")
+const paragraph = document.getElementById("para")
+const button = document.getElementById("btn")
+
+function dynamicUpdate(){
+    heading.innerText = "Welcome to DOM"
+    heading.style.color = "red"
+};
