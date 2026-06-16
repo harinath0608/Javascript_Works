6c26e3b2f88a31d611d6b047bc820a8af5c4f39d
harinath0608 <harinath192006@gmail.com>
2026-06-16 06:37:31 +0530

    DOM

---
+++ DOM/app.js
@@ -0,0 +1,14 @@
+// const Heading = document.getElementById("title")
+// const Heading = document.querySelector("#title")
+// const Heading = document.querySelector(".Title")
+const Heading = document.querySelectorAll("h1")
+
+Heading[0].innerText = "Query Selector All"
+Heading[0].style.color = "red"
+
+Heading[1].innerText = "DOM = Js"
+Heading[1].style.color = "blue"
+
+Heading.innerText = "Dynamic Content = DOM"
+Heading.style.color = "white"
+Heading.style.backgroundColor = "red"
