c3838628590a3eee4e8b4804ca42fd69162d50cd
harinath0608 <harinath192006@gmail.com>
2026-06-19 07:18:32 +0530

    validation completed

--- Mini_Project_FormValidation/app.js
+++ Mini_Project_FormValidation/app.js
@@ -3,12 +3,14 @@ const form = document.getElementById("registrationForm");
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
+    // access
     let name = document.getElementById("text").value.trim();
     let email = document.getElementById("email").value.trim();
     let phone = document.getElementById("number").value.trim();
     let password = document.getElementById("pass").value.trim();
 
 
+    // Patterns
     let nameRegex = /^[A-Za-z]+$/;
     let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
     let phoneRegex = /^[0-9]{10}$/
@@ -24,9 +26,24 @@ form.addEventListener("submit", function(event) {
     }
 
     // Email Regex Validation 
-    
+    if(!emailRegex.test(email)) {
+        document.getElementById("emailError").innerText = "Enter Valid Email"
+    } else {
+        document.getElementById("emailError").innerText = "";
+    }
 
+    // Phone Validation
+    if(!phoneRegex.test(phone)) {
+        document.getElementById("phoneError").innerText = "Enter 10 digit phone number";
+    } else {
+        document.getElementById("phoneError").innerText = "";
+    }
 
-
+    // Password Validation
+    if(!passwordRegex.test(password)) {
+        document.getElementById("passwordError").innerText = "Password must contain uppercase, lowercase, number and special characters"
+    } else {
+        document.getElementById("passwordError").innerText = "";
+    }
 
 })
